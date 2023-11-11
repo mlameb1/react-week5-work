daf7c226dbe6ee4dcb097d7790192c8b8ba1bcf6
mlameb1 <mlameb1@gmail.com>
2023-11-11 18:58:12 +0200

    added the GitHub link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import './App.css';
 export default function App() {
 return (
     <div className="App">
-
+Weather App
+<footer>
+  This project was coded by E Mlambo and is{""}
+  <a href = "https://github.com/mlameb1/react-week5-work"
+  target = "-blank">
+    open-sourced on GitHub
+  </a>
+</footer>
     </div>
   );
 }
