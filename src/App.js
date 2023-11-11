6db220d4c932bc7b6ced2b418d635ee900b9aefa
mlameb1 <mlameb1@gmail.com>
2023-11-11 19:42:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ return (
     <div className="App">
 <h1> Weather App</h1>
 <footer>
-
+<p>Coded by E Mlambo</p>
          <a
           className="App-link"
           href="https://github.com/mlameb1/my-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-         E Mlambo open-sourced GitHub link
+         open-sourced GitHub link
         </a>
+
         
 </footer>
     </div>
