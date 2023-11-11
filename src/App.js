99bd23d05792de09f444e9a9c63f500098695c76
mlameb1 <mlameb1@gmail.com>
2023-11-11 19:19:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ return (
     <div className="App">
 <h1> Weather App</h1>
 <footer>
-  This project was coded by E Mlambo and is{""}
-  <a href = "https://github.com/mlameb1/react-week5-work"
-  target = "-blank">
-    open-sourced on GitHub
-  </a>
+
+         <a
+          className="App-link"
+          href="https://github.com/mlameb1/my-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         E Mlambo open-sourced GitHub link
+        </a>
+        
 </footer>
     </div>
   );
