2450c61784b67f3686e2278b956f1ffc7032ce73
mlameb1 <mlameb1@gmail.com>
2023-11-12 11:10:08 +0200

    updated the css files and the js files

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,51 @@
 import React from "react";
-import "./weather.css";
+import "./Weather.css";
 
-export default function Weather(){
-    return <div>Weather App building..</div>;
+export default function Weather() {
+    return (<div className="weather">
+         <form>
+       <div className="row">
+        <div className="col-9">
+            <input type="search" 
+            placeholder="Enter a city.." 
+            className="form-control"
+            autofocus="on"
+            />
+            </div>  
+            <div className="col-3">
+                <input type="submit"
+                value="search"
+                className="btn btn-primary w-100"/>
+                </div>  
+                </div> 
+            </form>
+        <h1>New York</h1>
+        <ul>
+            <li>Wednesday 07:00</li>
+            <li>Mostly cloudy</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <div className="clearfix">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                    alt="Mostly Cloudy"
+                    className="float-left"></img>
+                    
+                    <div className="float-left">
+                        <span className="temperature">6</span>
+                        <span className="unit">°C</span>
+                    </div>
+                </div>
+                </div>
+            
+        <div className="col-6">
+            <ul>
+                <li>Precipitation: 15%</li>
+                <li>Humidity: 72%</li>
+                <li>Wind: 13km/h</li>
+            </ul>
+       </div>
+       </div>
+    </div>
+    );
 }
