18b6e6be4e0643cac36637055b0bee3b77c4bba5
mlameb1 <mlameb1@gmail.com>
2023-11-15 11:55:52 +0200

    made some corrections

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import FormattedDate from "./FormattedDate"
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./weather.css";
 
@@ -23,6 +23,8 @@ if(weatherData.ready){
 
     return (<div className="weather">
          <form>
+            <WeatherInfo data={weatherData}/>
+            
        <div className="row">
         <div className="col-9">
             <input type="search" 
@@ -38,33 +40,7 @@ if(weatherData.ready){
                 </div>  
                 </div> 
             </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-            <li><FormattedDate date = {weatherData.date}/></li>
-            <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-            <div className="col-6">
-                <div className="clearfix">
-                    <img src={weatherData.iconUrl}
-                    alt={weatherData.description}
-                    className="float-left"></img>
-                    
-                    <div className="float-left">
-                        <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                        <span className="unit">°C</span>
-                    </div>
-                </div>
-                </div>
-            
-        <div className="col-6">
-            <ul>
-            
-                <li>Humidity: {weatherData.humidity}%</li>
-                <li>Wind: {weatherData.wind}km/h</li>
-            </ul>
-       </div>
-       </div>
+        
     </div>
     );
 } else{
