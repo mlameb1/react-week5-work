ad07fe30c0ab0c29d4d7a7909fbee9ee5636257d
mlameb1 <mlameb1@gmail.com>
2023-11-15 11:16:21 +0200

    UpttedDate.js and the Weather.js filesdated the Forma

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import FormattedDate from "./FormattedDate"
 import axios from "axios";
 import "./weather.css";
 
@@ -11,7 +12,7 @@ setWeatherData({
  ready: true,
  temperature: response.data.main.temp,
  humidity: response.data.main.humidity,
- date: "Wednesday 07:00",
+ date: new Date(response.data.dt*1000),
  description: response.data.weather[0].description,
  iconUrl: 'https://ssl.sstatic.com/onebox/weather/64/partly_cloudy.png',
  wind: response.data.wind.speed,
@@ -39,7 +40,7 @@ if(weatherData.ready){
             </form>
         <h1>{weatherData.city}</h1>
         <ul>
-            <li>{weatherData.date}</li>
+            <li><FormattedDate date = {weatherData.date}/></li>
             <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
