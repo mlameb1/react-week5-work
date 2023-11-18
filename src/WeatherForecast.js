2c19d2da8b3c51ae04da01d609bf4001c96ae0ba
mlameb1 <mlameb1@gmail.com>
2023-11-18 18:37:06 +0200

    Corrected the apiUrl address

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -31,7 +31,7 @@ if (loaded){
 let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
 let longitude = props.coordinates.lon;
 let latitude = props.coordinates.lat;
-let apiUrl = `https//api.opnweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units="metric`
+let apiUrl = `https//api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
 axios.get(apiUrl).then(handleResponse);
 
 return null;
